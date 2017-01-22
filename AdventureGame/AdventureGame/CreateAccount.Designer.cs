551bb5c8c62e561f4cef809f40bcb16fd4107009
Vincent Hebble <vincent.hebble@laverne.edu>
2017-01-21 18:34:52 -0800

    Added Database and Create Account Page

---
+++ AdventureGame/AdventureGame/CreateAccount.Designer.cs
@@ -0,0 +1,164 @@
+﻿namespace AdventureGame
+{
+    partial class CreateAccount
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.usernameLabel = new System.Windows.Forms.Label();
+            this.passwordLabel = new System.Windows.Forms.Label();
+            this.usernameTextBox = new System.Windows.Forms.TextBox();
+            this.createButton = new System.Windows.Forms.Button();
+            this.clearButton = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.password1TextBox = new System.Windows.Forms.MaskedTextBox();
+            this.password2TextBox = new System.Windows.Forms.MaskedTextBox();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Transparent;
+            this.label1.Font = new System.Drawing.Font("Parchment", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(89, 29);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(670, 128);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Create Your Account";
+            // 
+            // usernameLabel
+            // 
+            this.usernameLabel.AutoSize = true;
+            this.usernameLabel.BackColor = System.Drawing.Color.Transparent;
+            this.usernameLabel.Font = new System.Drawing.Font("Parchment", 21.9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.usernameLabel.Location = new System.Drawing.Point(34, 178);
+            this.usernameLabel.Name = "usernameLabel";
+            this.usernameLabel.Size = new System.Drawing.Size(253, 78);
+            this.usernameLabel.TabIndex = 1;
+            this.usernameLabel.Text = "User Name";
+            // 
+            // passwordLabel
+            // 
+            this.passwordLabel.AutoSize = true;
+            this.passwordLabel.BackColor = System.Drawing.Color.Transparent;
+            this.passwordLabel.Font = new System.Drawing.Font("Parchment", 21.9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.passwordLabel.Location = new System.Drawing.Point(49, 256);
+            this.passwordLabel.Name = "passwordLabel";
+            this.passwordLabel.Size = new System.Drawing.Size(203, 78);
+            this.passwordLabel.TabIndex = 2;
+            this.passwordLabel.Text = "Password";
+            // 
+            // usernameTextBox
+            // 
+            this.usernameTextBox.Location = new System.Drawing.Point(335, 204);
+            this.usernameTextBox.Name = "usernameTextBox";
+            this.usernameTextBox.Size = new System.Drawing.Size(375, 38);
+            this.usernameTextBox.TabIndex = 0;
+            // 
+            // createButton
+            // 
+            this.createButton.Location = new System.Drawing.Point(324, 593);
+            this.createButton.Name = "createButton";
+            this.createButton.Size = new System.Drawing.Size(165, 64);
+            this.createButton.TabIndex = 3;
+            this.createButton.Text = "Create";
+            this.createButton.UseVisualStyleBackColor = true;
+            this.createButton.Click += new System.EventHandler(this.createButton_Click);
+            // 
+            // clearButton
+            // 
+            this.clearButton.Location = new System.Drawing.Point(513, 593);
+            this.clearButton.Name = "clearButton";
+            this.clearButton.Size = new System.Drawing.Size(165, 64);
+            this.clearButton.TabIndex = 4;
+            this.clearButton.Text = "Clear";
+            this.clearButton.UseVisualStyleBackColor = true;
+            this.clearButton.Click += new System.EventHandler(this.clearButton_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.Color.Transparent;
+            this.label2.Font = new System.Drawing.Font("Parchment", 21.9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(49, 353);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(203, 156);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "Retype\nPassword";
+            // 
+            // password1TextBox
+            // 
+            this.password1TextBox.Location = new System.Drawing.Point(335, 282);
+            this.password1TextBox.Name = "password1TextBox";
+            this.password1TextBox.PasswordChar = '*';
+            this.password1TextBox.Size = new System.Drawing.Size(375, 38);
+            this.password1TextBox.TabIndex = 8;
+            // 
+            // password2TextBox
+            // 
+            this.password2TextBox.Location = new System.Drawing.Point(335, 418);
+            this.password2TextBox.Name = "password2TextBox";
+            this.password2TextBox.PasswordChar = '*';
+            this.password2TextBox.Size = new System.Drawing.Size(375, 38);
+            this.password2TextBox.TabIndex = 9;
+            // 
+            // CreateAccount
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(16F, 31F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = global::AdventureGame.Properties.Resources.backgound;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(805, 744);
+            this.Controls.Add(this.password2TextBox);
+            this.Controls.Add(this.password1TextBox);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.clearButton);
+            this.Controls.Add(this.createButton);
+            this.Controls.Add(this.usernameTextBox);
+            this.Controls.Add(this.passwordLabel);
+            this.Controls.Add(this.usernameLabel);
+            this.Controls.Add(this.label1);
+            this.Name = "CreateAccount";
+            this.Text = "Create An Account";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label usernameLabel;
+        private System.Windows.Forms.Label passwordLabel;
+        private System.Windows.Forms.TextBox usernameTextBox;
+        private System.Windows.Forms.Button createButton;
+        private System.Windows.Forms.Button clearButton;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.MaskedTextBox password1TextBox;
+        private System.Windows.Forms.MaskedTextBox password2TextBox;
+    }
+}
